4f9de7c807076ed4e77362cb8af1ad256e3d40fd
skybosi <skybosi_ok@163.com>
2020-02-15 18:29:29 +0800

    "事件添加env"

--- template/render/form/form.js
+++ template/render/form/form.js
@@ -44,4 +44,55 @@ export default {
       }
     })
   },
+  stepper(e) {
+    this.beforeClick(e)
+    let eventOpts = e.currentTarget.dataset.eventOpts || e.target.dataset.eventOpts || {}
+    let value = eventOpts[0] || 0
+    let min = eventOpts[1] || 0
+    let max = eventOpts[2] || 10
+    let step = eventOpts[3] || 1
+    let type = eventOpts[4] || ''
+    let inputValue = e.detail.value
+    let precision = ((value + '.').split('.') || [0, 0])[1].length || 0
+    var scale = 1;
+    // 浮点型
+    if (~~step !== step) {
+      scale = Math.pow(10, (step + '').split('.')[1].length);
+    }
+    step = step * scale;
+    value = scale * value;
+    min = scale * min;
+    max = scale * max;
+    if (type === 'minus') {
+      value -= step;
+      value = parseFloat(value / scale).toFixed(precision)
+    } else if (type === 'plus') {
+      value += step;
+      value = parseFloat(value / scale).toFixed(precision)
+    } else if (type === 'input') {
+      // mark 不支持input等原生组件兼容
+      e.mark = {
+        tindex: e.currentTarget.dataset.tindex || e.target.dataset.tindex,
+        route: e.currentTarget.dataset.route || e.target.dataset.route
+      }
+      if (inputValue + '' != inputValue) {
+        value = scale * inputValue;
+      } else {
+        value = inputValue
+      }
+    }
+    if (value < min || value > max) {
+      return;
+    }
+    this.renderData(e, {
+      value: value
+    })
+  },
+  clickRate(e) {
+    this.beforeClick(e)
+    let index = e.currentTarget.dataset.index || e.target.dataset.index || 0
+    this.renderData(e, {
+      value: index + 1
+    })
+  },
 }
